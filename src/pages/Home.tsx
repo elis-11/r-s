e203085e1ad7b0b859b26ba741a85eace005cc68
elis-11 <eliza.arzanukaeva@gmail.com>
2022-10-07 18:05:03 +0200

    Redux - first-async-res-going-to github

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSearchUsersQuery } from '../store/github/github.api'
 
 export const Home = () => {
+
+const { isLoading, isError, data} = useSearchUsersQuery("elis-11")
+
   return (
     <div>Home</div>
   )
