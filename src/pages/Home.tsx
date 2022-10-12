4faa351ea7c440b49e62669a6b81e8f23bb7b620
elis-11 <eliza.arzanukaeva@gmail.com>
2022-10-12 12:02:14 +0200

    Dropdown & request to the github

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,15 @@ import { useSearchUsersQuery } from "../store/github/github.api";
 
 export const Home = () => {
   const [search, setSearch] = useState("");
-  const debounced = useDebounce(search)
-  const { isLoading, isError, data } = useSearchUsersQuery("elis-1");
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+  });
 
   useEffect(() => {
-    console.log(debounced);
-  }, [debounced]);
+setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data]);
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -19,16 +22,25 @@ export const Home = () => {
 
       <div className="relative w-[560px]">
         <input
-          autoFocus 
+          autoFocus
           type="text"
           className="border py-2 px-4 w-full h-[42px] mb-2"
           placeholder="Search for github username..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis?
-        </div>
+        {dropdown && <ul className="absolute list-none top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+          {isLoading && <p className="text-center">Loading...</p>}
+          {data?.map((user) => (
+            <li
+              key={user.id}
+              className="py-2 px-4 hover:bg-gray-300 hover:text-white transition-colors cursor-pointer"
+            >
+              {" "}
+              {user.login}
+            </li>
+          ))}
+        </ul>}
       </div>
     </div>
   );
